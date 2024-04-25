ae141f7739f4b8bb4f056f224f06751d75ef64d9
Maor Somech <maorsom@gmail.com>
2024-04-26 00:08:52 +0300

    initial commit

---
+++ lua/maorsom/plugins/colorscheme.lua
@@ -0,0 +1,18 @@
+return {
+  "catppuccin/nvim",
+  priority = 1000,
+  config = function()
+   
+    require("catppuccin").setup({
+      flavour = "mocha",
+      dim_inactive = {
+        enabled = true,
+        shade = "dark",
+        percentage = 0.15
+      }
+    })
+
+
+    vim.cmd("colorscheme catppuccin")
+  end
+}
