ad89508da6873cccad0bee7100ca71b49ab37640
Maor Somech <maorsom@gmail.com>
2024-04-26 04:10:52 +0300

    first setup

--- lua/maorsom/core/options.lua
+++ lua/maorsom/core/options.lua
@@ -1,20 +1,19 @@
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
 vim.g.have_nerd_font = true
-
 
 -- Make line number on the left
 vim.opt.relativenumber = true
 vim.opt.number = true
 
 --Enable mouse
-vim.opt.mouse = 'a'
+vim.opt.mouse = "a"
 
 -- Disable show mode,  will be visibale on statusline
 vim.opt.showmode = false
 
-vim.opt.clipboard = 'unnamedplus'
+vim.opt.clipboard = "unnamedplus"
 
 vim.opt.breakindent = true
 
@@ -23,7 +22,7 @@ vim.opt.undofile = true
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 
-vim.opt.signcolumn = 'yes'
+vim.opt.signcolumn = "yes"
 
 vim.opt.updatetime = 250
 
@@ -33,18 +32,18 @@ vim.opt.splitright = true
 vim.opt.splitbelow = true
 
 vim.opt.list = true
-vim.opt.listchars = { tab =  '» ', trail = '·', nbsp = '␣'}
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
 
-vim.opt.inccommand = 'split'
+vim.opt.inccommand = "split"
 
 vim.opt.cursorline = true
 
 vim.opt.scrolloff = 10
 
-vim.opt.tabstop = 2 -- spaces for tabs
-vim.opt.shiftwidth = 2 -- space for indent width
-vim.opt.expandtab = true -- expand tab to spaces
-vim.opt.autoindent = true -- copy indent from current line when starting new line
+-- vim.opt.tabstop = 2 -- spaces for tabs
+-- vim.opt.shiftwidth = 2 -- space for indent width
+-- vim.opt.expandtab = true -- expand tab to spaces
+-- vim.opt.autoindent = true -- copy indent from current line when starting new line
 
 vim.opt.wrap = false
 
@@ -52,3 +51,4 @@ vim.opt.swapfile = false
 
 vim.opt.hlsearch = true
 
+vim.filetype.add({ extension = { templ = "templ" } })
